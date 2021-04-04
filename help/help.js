74baf8dbc09abcf2503c3af66c33bdc8b8d8dc40
itsmeandi <78503174+itsmeandi@users.noreply.github.com>
2021-04-04 20:52:41 +0800

    Update help.js

--- help/help.js
+++ help/help.js
@@ -10,58 +10,7 @@ const help = (prefix) => {
 ├ • ${prefix}tagstick
 ├ • ${prefix}clearall
 │
-├──「 Islami 」
-├ • ${prefix}listsurah
-├ • ${prefix}asmaulhusna
-├ • ${prefix}alquran no_surah
-├ • ${prefix}alquran no_surah/no_ayat
-├ • ${prefix}alquran no_surah/no_ayat1-no_ayat2
-├ • ${prefix}alquranaudio no_surah
-├ • ${prefix}alquranaudio no_surah/no_ayat
-├ • ${prefix}kisahnabi nama_nabi
-├ • ${prefix}jadwalsholat daerah
-│
-├──「 Downloader 」
-├ • ${prefix}ytsearch query
-├ • ${prefix}ytplay query
-├ • ${prefix}ytplay2 query
-├ • ${prefix}ytmp3 url_video
-├ • ${prefix}ytmp32 url_video
-├ • ${prefix}ytmp4 url_video
-├ • ${prefix}ytmp42 url_video
-├ • ${prefix}tiktoknowm url_video
-├ • ${prefix}tiktokmusic url_video
-├ • ${prefix}igdl url_post
-├ • ${prefix}fbdl url_video
-├ • ${prefix}jooxplay query
-├ • ${prefix}spotify url_music
-├ • ${prefix}spotifysearch query
-├ • ${prefix}pinterest query
-├ • ${prefix}pinterestdl url_pinterest
-├ • ${prefix}pixiv query
-├ • ${prefix}pixivdl url_pixiv
-├ • ${prefix}zippyshare url_zippyshare
-├ • ${prefix}telesticker url_pack
-│
-├──「 Movie & Story 」
-├ • ${prefix}drakorongoing
-├ • ${prefix}lk21 query
-├ • ${prefix}wattpad url_wattpad
-├ • ${prefix}wattpadsearch query
-├ • ${prefix}cerpen
-├ • ${prefix}ceritahoror
-│
-├──「 Searching 」
-├ • ${prefix}shopee query
-├ • ${prefix}google query
-├ • ${prefix}gimage query
-├ • ${prefix}gimage2 query
-├ • ${prefix}konachan query
-├ • ${prefix}playstore query
-├ • ${prefix}stickerwa query
-├ • ${prefix}wallpapersearch query
-├ • ${prefix}wallpapersearch2 query
-│
+
 ├──「 Random Text 」
 ├ • ${prefix}quotes
 ├ • ${prefix}quotesdilan
